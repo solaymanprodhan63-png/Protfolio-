48a490b4110aeb3635211c6035dc1d719bdf044b
solaymanprodhan63-png <solaymanprodhan63@gmail.com>
2026-03-05 23:11:56 +0600

    Add files via upload

---
+++ style.css
@@ -0,0 +1,57 @@
+body{
+font-family:Poppins;
+background:#05050a;
+color:white;
+line-height:1.8;
+margin:0;
+}
+
+nav{
+background:#111;
+padding:15px;
+text-align:center;
+position:sticky;
+top:0;
+}
+
+nav a{
+color:#00f2ff;
+margin:10px;
+text-decoration:none;
+font-weight:500;
+}
+
+section{
+max-width:1000px;
+margin:auto;
+padding:80px 20px;
+}
+
+.profile{
+width:180px;
+border-radius:50%;
+border:4px solid #00f2ff;
+}
+
+.gallery{
+display:grid;
+grid-template-columns:repeat(auto-fit,minmax(250px,1fr));
+gap:20px;
+}
+
+.gallery img{
+width:100%;
+border-radius:10px;
+transition:.3s;
+}
+
+.gallery img:hover{
+transform:scale(1.05);
+}
+
+.quote{
+border-left:4px solid #00f2ff;
+padding-left:15px;
+margin:20px 0;
+font-style:italic;
+}
